a52fefa9eada7f0651ac1626ca232e928bd6203c
gekomad <info@cinnamon.com>
2015-10-22 19:06:37 +0200

    .

--- src/util/logger.h
+++ src/util/logger.h
@@ -21,7 +21,6 @@
 #include "../util/Singleton.h"
 
 using namespace std;
-using namespace _def;
 
 #if !defined DLOG_LEVEL
 #if defined DEBUG_MODE
@@ -31,8 +30,13 @@ using namespace _def;
 #endif
 #endif
 
-
 namespace _logger {
+
+    static enum LOG_LEVEL {
+        TRACE = 0, DEBUG = 1, INFO = 2, WARN = 3, ERROR = 4, FATAL = 5, OFF = 6, ALWAYS = 7
+    } _LOG_LEVEL;
+
+    static const string LOG_LEVEL_STRING[] = {"TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL", "OFF", "LOG"};
 
     class Logger : public Singleton<Logger>, public ofstream {
         friend class Singleton<Logger>;
@@ -42,19 +46,38 @@ namespace _logger {
             this->open(f, append ? std::ofstream::app : std::ofstream::out);
         }
 
+        template<LOG_LEVEL type, typename T, typename... Args>
+        void _log(T t, Args... args) {
+            lock_guard<mutex> lock1(_CoutSyncMutex);
+            cout << Time::getLocalTime() << " " << LOG_LEVEL_STRING[type] << " ";
+            *this << Time::getLocalTime() << " " << LOG_LEVEL_STRING[type] << " ";
+            __log(t, args...);
+            cout << endl;
+            *this << endl;
+        }
+
     private:
+        mutex _CoutSyncMutex;
+
+        template<typename T>
+        void __log(T t) {
+            cout << (t);
+            *this << (t);
+        }
+
+        template<typename T, typename... Args>
+        void __log(T t, Args... args) {
+            cout << (t);
+            *this << (t);
+            __log(args...);
+        }
+
         ~Logger() {
             this->close();
         }
     };
 
     static Logger &logger = Logger::getInstance();
-    static mutex _CoutSyncMutex;
-
-    static enum LOG_LEVEL {
-        TRACE = 0, DEBUG = 1, INFO = 2, WARN = 3, ERROR = 4, FATAL = 5, OFF = 6, ALWAYS = 7
-    } _LOG_LEVEL;
-    static const string LOG_LEVEL_STRING[] = {"TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL", "OFF", "LOG"};
 
 #if defined(_WIN32)
 #define __FILENAME__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
@@ -64,35 +87,12 @@ namespace _logger {
 
 #define LINE_INFO __FILENAME__,":",__LINE__," "
 
-    template<typename T>
-    void __log(T t) {
-        cout << (t);
-        logger << (t);
-    }
-
-    template<typename T, typename... Args>
-    void __log(T t, Args... args) {
-        cout << (t);
-        logger << (t);
-        __log(args...);
-    }
-
-    template<LOG_LEVEL type, typename T, typename... Args>
-    void _log(T t, Args... args) {
-        lock_guard<mutex> lock1(_CoutSyncMutex);
-        cout << Time::getLocalTime() << " " << LOG_LEVEL_STRING[type] << " ";
-        logger << Time::getLocalTime() << " " << LOG_LEVEL_STRING[type] << " ";
-        __log(t, args...);
-        cout << endl;
-        logger << endl;
-    }
-
-#define log(...)                            {_log<LOG_LEVEL::ALWAYS>(LINE_INFO,__VA_ARGS__);}
-#define trace(...) if (TRACE >= DLOG_LEVEL) {_log<LOG_LEVEL::TRACE>( LINE_INFO,__VA_ARGS__);}
-#define debug(...) if (DEBUG >= DLOG_LEVEL) {_log<LOG_LEVEL::DEBUG>( LINE_INFO,__VA_ARGS__);}
-#define info(...)  if (INFO  >= DLOG_LEVEL) {_log<LOG_LEVEL::INFO> ( LINE_INFO,__VA_ARGS__);}
-#define warn(...)  if (WARN  >= DLOG_LEVEL) {_log<LOG_LEVEL::WARN> ( LINE_INFO,__VA_ARGS__);}
-#define error(...) if (ERROR >= DLOG_LEVEL) {_log<LOG_LEVEL::ERROR>( LINE_INFO,__VA_ARGS__);}
-#define fatal(...) if (FATAL >= DLOG_LEVEL) {_log<LOG_LEVEL::FATAL>( LINE_INFO,__VA_ARGS__);}
+#define log(...)                            {logger._log<LOG_LEVEL::ALWAYS>(LINE_INFO,__VA_ARGS__);}
+#define trace(...) if (TRACE >= DLOG_LEVEL) {logger._log<LOG_LEVEL::TRACE>( LINE_INFO,__VA_ARGS__);}
+#define debug(...) if (DEBUG >= DLOG_LEVEL) {logger._log<LOG_LEVEL::DEBUG>( LINE_INFO,__VA_ARGS__);}
+#define info(...)  if (INFO  >= DLOG_LEVEL) {logger._log<LOG_LEVEL::INFO> ( LINE_INFO,__VA_ARGS__);}
+#define warn(...)  if (WARN  >= DLOG_LEVEL) {logger._log<LOG_LEVEL::WARN> ( LINE_INFO,__VA_ARGS__);}
+#define error(...) if (ERROR >= DLOG_LEVEL) {logger._log<LOG_LEVEL::ERROR>( LINE_INFO,__VA_ARGS__);}
+#define fatal(...) if (FATAL >= DLOG_LEVEL) {logger._log<LOG_LEVEL::FATAL>( LINE_INFO,__VA_ARGS__);}
 
 }
