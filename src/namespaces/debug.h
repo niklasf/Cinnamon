85e5862272cd0b4676b03fedf53c71f9ec00e8af
geko <geko@geko-HP-Compaq-6005-Pro-SFF-PC.seat.it>
2015-10-22 17:10:04 +0200

    .

--- src/namespaces/debug.h
+++ src/namespaces/debug.h
@@ -30,7 +30,7 @@
 #endif
 #endif
 namespace _debug {
-
+aggiungere logger
     static enum LOG_LEVEL {
         TRACE = 0, DEBUG = 1, INFO = 2, WARN = 3, ERROR = 4, FATAL = 5, OFF = 6
     } _LOG_LEVEL;
