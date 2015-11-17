73be9d9410967c14008e5cbb745ac042bf9c9cbb
geko <geko@geko-HP-Compaq-6005-Pro-SFF-PC.seat.it>
2015-11-17 09:50:33 +0100

    .

--- src/perft/PerftThread.cpp
+++ src/perft/PerftThread.cpp
@@ -22,10 +22,10 @@
 Mutex PerftThread::MUTEX_HASH;
 mutex PerftThread::mutexPrint;
 
-PerftThread::PerftThread() { }
+PerftThread::PerftThread() { perftMode = true;}
 
 void PerftThread::setParam(string fen1, int from1, int to1, _TPerftRes *perft1) {
-    perftMode = true;
+
     loadFen(fen1);
     this->tPerftRes = perft1;
     this->from = from1;
@@ -34,7 +34,6 @@ void PerftThread::setParam(string fen1, int from1, int to1, _TPerftRes *perft1) 
 
 vector<string> PerftThread::getSuccessorsFen(const string &fen1, const int depth) {
     loadFen(fen1);
-    setForceCheck(true);
     if (getSide()) return getSuccessorsFen<WHITE>(depth);
     return getSuccessorsFen<BLACK>(depth);
 }
